d36c9de6e64ca6f7fb759ee17d552ac9be83875e
YuriMascarenhasLourenco <lourenco99yuri@gmail.com>
2026-01-04 21:15:30 -0300

    notification send event created

---
+++ libs/events/src/events/notification-send-v1.event.ts
@@ -0,0 +1,11 @@
+export const NOTIFICATION_SEND_V1 = 'notification.send.v1' as const;
+
+export type NotificationSendV1 = {
+  eventId: string;
+  occurredAt: string;
+  payload: {
+    name: string;
+    quantity: number;
+    price: number;
+  };
+};
