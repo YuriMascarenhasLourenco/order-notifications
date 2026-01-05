bc51f6f663e48fee6ca81f9b650ab35cfe119848
YuriMascarenhasLourenco <lourenco99yuri@gmail.com>
2026-01-04 21:43:46 -0300

    index.ts event lib

---
+++ libs/events/src/index.ts
@@ -0,0 +1,5 @@
+export * from './events.module';
+export * from './events.service';
+export * from './events/notification-send-v1.event';
+export * from './events/order-created-v1.event';
+export * from './events/payment-process-v1.event';
