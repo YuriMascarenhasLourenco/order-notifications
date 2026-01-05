635d47cad4f52d3626247909aef1fab6a196b6e7
YuriMascarenhasLourenco <lourenco99yuri@gmail.com>
2026-01-04 21:06:13 -0300

    order event added

--- apps/api-gateway/src/api-gateway.controller.ts
+++ apps/api-gateway/src/api-gateway.controller.ts
@@ -2,7 +2,9 @@ import { Body, Controller, Inject, Post } from '@nestjs/common';
 import { ApiGatewayService } from './api-gateway.service';
 import { ORDER_SERVICE } from '../constant';
 import { ClientProxy } from '@nestjs/microservices';
-import { ORDER_MESSAGE } from 'constant';
+import { CreateOrderDto } from 'lib/my-library';
+import { ORDER_CREATED_V1, OrderCreatedV1 } from 'event-lib/events';
+import { randomUUID } from 'crypto';
 
 @Controller()
 export class ApiGatewayController {
@@ -11,8 +13,17 @@ export class ApiGatewayController {
     @Inject(ORDER_SERVICE) private readonly RMQOrderClient: ClientProxy,
   ) {}
   @Post('order')
-  createOrder(@Body() orderData: any) {
-    this.RMQOrderClient.emit(ORDER_MESSAGE, orderData);
+  async createOrder(@Body() orderData: CreateOrderDto) {
+    const newOrderEvent: OrderCreatedV1 = {
+      eventId: randomUUID(),
+      occurredAt: new Date().toISOString(),
+      payload: {
+        name: orderData.name,
+        quantity: orderData.quantity,
+        price: orderData.price,
+      },
+    };
+    await this.RMQOrderClient.emit(ORDER_CREATED_V1, newOrderEvent);
     return { message: 'Order creation request sent.' };
   }
 }
