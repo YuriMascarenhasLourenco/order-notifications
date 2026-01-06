2272c6487bd172ea38267ae8aa2cd6093a91cfa2
YuriMascarenhasLourenco <lourenco99yuri@gmail.com>
2026-01-05 22:03:08 -0300

    email notification method created

--- apps/order-notifications/src/app.service.ts
+++ apps/order-notifications/src/app.service.ts
@@ -1,8 +1,12 @@
+import type { NotificationSendV1 } from '@lib/events';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+  sendNotification(notification: NotificationSendV1): Promise<void> {
+    return Promise.resolve();
   }
 }
