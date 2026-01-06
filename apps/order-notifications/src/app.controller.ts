8b342a98e6484bd247c3a436af349995b989e2f6
YuriMascarenhasLourenco <lourenco99yuri@gmail.com>
2026-01-05 22:01:35 -0300

    message pattern and event type imported

--- apps/order-notifications/src/app.controller.ts
+++ apps/order-notifications/src/app.controller.ts
@@ -1,15 +1,16 @@
 import { Controller } from '@nestjs/common';
 import { AppService } from './app.service';
 import { EventPattern } from '@nestjs/microservices';
-import { NOTIFICATION_MESSAGE } from 'constant';
+import { NOTIFICATION_SEND_V1, type NotificationSendV1 } from '@lib/events';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @EventPattern(NOTIFICATION_MESSAGE)
-  handleSendNotification(order: any) {
+  @EventPattern(NOTIFICATION_SEND_V1)
+ async handleSendNotification(order: NotificationSendV1) {
     console.log('Notification Event Received:', order);
     // Lógica para enviar notificação (e.g., email, SMS, etc.)
+    await this.appService.sendNotification(order);
   }
 }
