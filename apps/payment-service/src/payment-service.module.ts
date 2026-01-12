1067cf3a5620e288b4270b53da52f02467d3f56c
YuriMascarenhasLourenco <lourenco99yuri@gmail.com>
2026-01-12 13:28:28 -0300

    payment-service module with envFilePath and Stripe provider

--- apps/payment-service/src/payment-service.module.ts
+++ apps/payment-service/src/payment-service.module.ts
@@ -1,11 +1,28 @@
 import { Module } from '@nestjs/common';
 import { PaymentServiceController } from './payment-service.controller';
 import { PaymentServiceService } from './payment-service.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { join } from 'path';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: [
+        join(process.cwd(), '.env'), // raiz
+        join(process.cwd(), 'apps/payment-service/.env.payment'), // específico
+      ],
+    }),
+  ],
   controllers: [PaymentServiceController],
-  providers: [PaymentServiceService],
+  providers: [
+    PaymentServiceService,
+    {
+      provide: 'STRIPE_API_KEY',
+      useFactory: (configService: ConfigService) =>
+        configService.getOrThrow<string>('STRIPE_SECRET_KEY'),
+      inject: [ConfigService],
+    },
+  ],
 })
 export class PaymentServiceModule {}
