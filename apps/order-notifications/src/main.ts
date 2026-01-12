1c5cf37e84849b0acb0de1f09c9d36cc785577b3
YuriMascarenhasLourenco <lourenco99yuri@gmail.com>
2026-01-12 13:33:59 -0300

    createRmqMicroserviceOptions added

--- apps/order-notifications/src/main.ts
+++ apps/order-notifications/src/main.ts
@@ -1,30 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { MicroserviceOptions } from '@nestjs/microservices';
 import { Logger } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import { NOTIFICATION_QUEUE } from 'constant';
+import { createRmqMicroserviceOptions } from 'rabbitmq/rabbitmq/config/microservice/create-microservice';
+import { QUEUES } from '@lib/events';
 
 async function bootstrap() {
-  const appContext = await NestFactory.createApplicationContext(AppModule);
-  const config = appContext.get(ConfigService);
-
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
     AppModule,
-    {
-      transport: Transport.RMQ,
-      options: {
-        urls: [config.getOrThrow<string>('RABBITMQ_URL')],
-        queue: NOTIFICATION_QUEUE,
-        queueOptions: { durable: true },
-      },
-    },
+    createRmqMicroserviceOptions(QUEUES.NOTIFICATION),
   );
 
   await app.listen();
   Logger.log('Notifications microservice is listening on RabbitMQ...');
-
-  await appContext.close();
 }
 bootstrap().catch((error) => {
   Logger.error('Error starting notifications microservice', error);
