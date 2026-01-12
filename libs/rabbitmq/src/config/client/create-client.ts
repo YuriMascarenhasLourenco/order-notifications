0207cb900588c6f39548de332f99a6e4423ea5cf
YuriMascarenhasLourenco <lourenco99yuri@gmail.com>
2026-01-12 13:25:48 -0300

    creatermqCliente with configService now

--- libs/rabbitmq/src/config/client/create-client.ts
+++ libs/rabbitmq/src/config/client/create-client.ts
@@ -1,11 +1,12 @@
 import { ClientProxyFactory, Transport } from '@nestjs/microservices';
 import { RMQ_DEFAULTS } from '../options/rmq-options';
+import { ConfigService } from '@nestjs/config';
 
-export function createRmqClient(queue: string) {
+export function createRmqClient(config: ConfigService, queue: string) {
   return ClientProxyFactory.create({
     transport: Transport.RMQ,
     options: {
-      urls: [process.env.RABBITMQ_URL!],
+      urls: [config.getOrThrow<string>('RABBITMQ_URL')],
       queue,
       queueOptions: RMQ_DEFAULTS.queueOptions,
     },
