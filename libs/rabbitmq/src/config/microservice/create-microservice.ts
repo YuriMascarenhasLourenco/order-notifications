a527e22127f13d50797ef3b342c899f973b0a77e
YuriMascarenhasLourenco <lourenco99yuri@gmail.com>
2026-01-12 13:24:39 -0300

    microservice configService added

--- libs/rabbitmq/src/config/microservice/create-microservice.ts
+++ libs/rabbitmq/src/config/microservice/create-microservice.ts
@@ -1,13 +1,20 @@
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { RMQ_DEFAULTS } from '../options/rmq-options';
+import { ConfigService } from '@nestjs/config';
 
 export function createRmqMicroserviceOptions(
   queue: string,
 ): MicroserviceOptions {
+  const config = new ConfigService();
+  const url = config.getOrThrow<string>('RABBITMQ_URL');
+  if (!url) {
+    throw new Error('RABBITMQ_URL is not defined');
+  }
+
   return {
     transport: Transport.RMQ,
     options: {
-      urls: [process.env.RABBITMQ_URL!],
+      urls: [url],
       queue,
       queueOptions: RMQ_DEFAULTS.queueOptions,
       noAck: RMQ_DEFAULTS.noAck,
