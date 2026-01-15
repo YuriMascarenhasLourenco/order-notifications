73b97578c400965e457c4ac301b7fa21ac475146
YuriMascarenhasLourenco <lourenco99yuri@gmail.com>
2026-01-15 19:00:51 -0300

    ORDER_QUEUE,NOTIFICATION_QUEUE,PAYMENT_QUEUE,ORDER_MESSAGE,NOTIFICATION_MESSAGE, PAYMENT_MESSAGE deleted
    stripe consts added

--- constant.ts
+++ constant.ts
@@ -1,6 +1,2 @@
-export const ORDER_QUEUE = 'order-queue';
-export const NOTIFICATION_QUEUE = 'notification-queue';
-export const PAYMENT_QUEUE = 'payment-queue';
-export const ORDER_MESSAGE = 'order.created';
-export const NOTIFICATION_MESSAGE = 'notification.send';
-export const PAYMENT_MESSAGE = 'payment.process';
+export const STRIPE_SECRET_KEY = 'STRIPE_SECRET_KEY';
+export const STRIPE_API_VERSION = 'STRIPE_API_VERSION';
