8ea32066f6d0a6299332d5a5018bb1e6c373673f
YuriMascarenhasLourenco <lourenco99yuri@gmail.com>
2026-01-15 18:51:20 -0300

    typeorm module, QUEUES, RMQClients, orderEntity and .env files added

--- apps/order-service/src/order-service.module.ts
+++ apps/order-service/src/order-service.module.ts
@@ -1,42 +1,63 @@
 import { Module } from '@nestjs/common';
 import { ClientsModule, Transport } from '@nestjs/microservices';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-
 import { OrderServiceController } from './order-service.controller';
 import { OrderServiceService } from './order-service.service';
-
-import { NOTIFICATION_SERVICE, PAYMENT_SERVICE } from '../constant';
-import { NOTIFICATION_QUEUE, PAYMENT_QUEUE } from 'constant';
+import { QUEUES, RMQClients } from '@lib/events';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
+import { OrderEntity } from './entities/orderRepository';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      envFilePath: [
+        join(process.cwd(), '.env'),
+        join(process.cwd(), 'apps/order-service/.env.order'),
+      ],
     }),
+
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.getOrThrow<string>('DB_HOST'),
+        port: config.getOrThrow<number>('DB_PORT'),
+        username: config.getOrThrow<string>('DB_USERNAME'),
+        password: config.getOrThrow<string>('DB_PASSWORD'),
+        database: config.getOrThrow<string>('DB_DATABASE'),
+        entities: [OrderEntity],
+        synchronize: true,
+      }),
+    }),
+    TypeOrmModule.forFeature([OrderEntity]),
 
     ClientsModule.registerAsync([
       {
-        name: NOTIFICATION_SERVICE,
-        imports: [ConfigModule], // garante ConfigService disponível aqui
+        name: RMQClients.NOTIFICATION_SERVICE,
+        imports: [ConfigModule],
         inject: [ConfigService],
         useFactory: (config: ConfigService) => ({
           transport: Transport.RMQ,
           options: {
             urls: [config.getOrThrow<string>('RABBITMQ_URL')],
-            queue: NOTIFICATION_QUEUE,
+            queue: QUEUES.NOTIFICATION,
             queueOptions: { durable: true },
+            noAck: true,
           },
         }),
       },
       {
-        name: PAYMENT_SERVICE,
-        imports: [ConfigModule], // garante ConfigService disponível aqui
+        name: RMQClients.PAYMENT_SERVICE,
+        imports: [ConfigModule],
         inject: [ConfigService],
         useFactory: (config: ConfigService) => ({
           transport: Transport.RMQ,
           options: {
             urls: [config.getOrThrow<string>('RABBITMQ_URL')],
-            queue: PAYMENT_QUEUE,
+            queue: QUEUES.PAYMENT,
             queueOptions: { durable: true },
           },
         }),
