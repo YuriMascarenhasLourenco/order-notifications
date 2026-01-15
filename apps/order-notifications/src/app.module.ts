bc4809d3ac2e8fff6951a7cd183910b28a1632fe
YuriMascarenhasLourenco <lourenco99yuri@gmail.com>
2026-01-15 18:41:10 -0300

    configModule, Config Service and mailer module added

--- apps/order-notifications/src/app.module.ts
+++ apps/order-notifications/src/app.module.ts
@@ -1,10 +1,32 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
-
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { join } from 'path';
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: [
+        join(process.cwd(), '.env'), // raiz
+        join(process.cwd(), 'apps/order-notifications/.env.notification'), // específico
+      ],
+    }),
+    MailerModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        transport: {
+          host: configService.getOrThrow<string>('SMTP_HOST'),
+          port: configService.getOrThrow<number>('SMTP_PORT'),
+          auth: {
+            user: configService.getOrThrow<string>('SMTP_USER'),
+            pass: configService.getOrThrow<string>('SMTP_PASS'),
+          },
+        },
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
