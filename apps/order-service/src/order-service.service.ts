e85045e7747bc39df07713a1f4a4d31e8410ac50
YuriMascarenhasLourenco <lourenco99yuri@gmail.com>
2026-01-15 18:52:36 -0300

    service methods to persist data on db created. get hello deleted

--- apps/order-service/src/order-service.service.ts
+++ apps/order-service/src/order-service.service.ts
@@ -1,8 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { OrderEntity } from './entities/orderRepository';
+import { Repository } from 'typeorm';
+import { OrderCreatedV1 } from '@lib/events';
 
 @Injectable()
 export class OrderServiceService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(
+    @InjectRepository(OrderEntity)
+    private readonly orderRepository: Repository<OrderEntity>,
+  ) {}
+  async createOrder(order: OrderCreatedV1) {
+    try {
+      const orderEntity = this.orderRepository.create({
+        eventId: order.eventId,
+        name: order.payload.name,
+        price: order.payload.price,
+        quantity: order.payload.quantity,
+      });
+      await this.orderRepository.save(orderEntity);
+      console.log(`Order created: ${orderEntity.eventId}`);
+    } catch (error) {
+      console.error('Error creating order:', error);
+    }
   }
 }
