d6c4f4e06a48c0754c9364b774c16e36036f06cf
YuriMascarenhasLourenco <lourenco99yuri@gmail.com>
2026-01-15 16:24:30 -0300

    swagger added to api gateway

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { ApiGatewayModule } from './api-gateway.module';
 import { Logger } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(ApiGatewayModule);
-  await app.listen(process.env.port ?? 3000);
+  const config = new DocumentBuilder()
+    .setTitle('API Gateway')
+    .setDescription('API Gateway for Order Processing System')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  await app.listen(3000);
   Logger.log('API Gateway is running on http://localhost:3000');
 }
-bootstrap();
+bootstrap().catch((error) => {
+  Logger.error('Error starting API Gateway', error);
+});
