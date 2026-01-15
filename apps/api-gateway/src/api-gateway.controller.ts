393992d76ef8376f123160baf3361e1897914474
YuriMascarenhasLourenco <lourenco99yuri@gmail.com>
2026-01-15 19:04:34 -0300

    api-gateway controller with swagger documentation

--- apps/api-gateway/src/api-gateway.controller.ts
+++ apps/api-gateway/src/api-gateway.controller.ts
@@ -1,21 +1,36 @@
 import { Body, Controller, Inject, Post } from '@nestjs/common';
 import { ApiGatewayService } from './api-gateway.service';
-import { ORDER_SERVICE } from '../constant';
 import { ClientProxy } from '@nestjs/microservices';
-
-
 import { randomUUID } from 'crypto';
 import { CreateOrderDto } from '@lib/my-library';
-import { ORDER_CREATED_V1, type OrderCreatedV1 } from '@lib/events';
+import { ORDER_CREATED_V1, RMQClients, type OrderCreatedV1 } from '@lib/events';
+import { ApiBody, ApiOperation } from '@nestjs/swagger';
 
 @Controller()
 export class ApiGatewayController {
   constructor(
     private readonly apiGatewayService: ApiGatewayService,
-    @Inject(ORDER_SERVICE) private readonly RMQOrderClient: ClientProxy,
+    @Inject(RMQClients.ORDER_SERVICE)
+    private readonly RMQOrderClient: ClientProxy,
   ) {}
+  @ApiOperation({ summary: 'Create a new order' })
+  @ApiBody({
+    type: CreateOrderDto,
+    required: true,
+    description: 'Data required to create a new order',
+    examples: {
+      example1: {
+        summary: 'Example order data',
+        value: {
+          name: 'Product Name',
+          quantity: 10,
+          price: 25.99,
+        },
+      },
+    },
+  })
   @Post('order')
-  async createOrder(@Body() orderData: CreateOrderDto) {
+  createOrder(@Body() orderData: CreateOrderDto) {
     const newOrderEvent: OrderCreatedV1 = {
       eventId: randomUUID(),
       occurredAt: new Date().toISOString(),
@@ -25,7 +40,7 @@ export class ApiGatewayController {
         price: orderData.price,
       },
     };
-    await this.RMQOrderClient.emit(ORDER_CREATED_V1, newOrderEvent);
+    this.RMQOrderClient.emit(ORDER_CREATED_V1, newOrderEvent);
     return { message: 'Order creation request sent.' };
   }
 }
