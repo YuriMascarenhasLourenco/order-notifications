ce2ff1fb100f6a31f458a159e504e20c6bdfbc23
YuriMascarenhasLourenco <lourenco99yuri@gmail.com>
2026-01-15 18:56:36 -0300

    sendNotification smtp method implemented

--- apps/order-notifications/src/app.service.ts
+++ apps/order-notifications/src/app.service.ts
@@ -1,12 +1,22 @@
-import type { NotificationSendV1 } from '@lib/events';
 import { Injectable } from '@nestjs/common';
+import { MailerService } from '@nestjs-modules/mailer';
+import { OrderCreatedV1 } from '@lib/events';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
-  }
-  sendNotification(notification: NotificationSendV1): Promise<void> {
-    return Promise.resolve();
+  constructor(private readonly mailer: MailerService) {}
+
+  async sendNotification(order: OrderCreatedV1): Promise<void> {
+    try {
+      const mail = await this.mailer.sendMail({
+        to: 'mascarenhasyuri3@gmail.com',
+        subject: `Seu pedido, ${order.eventId} está sendo processado!`,
+        template: './welcome',
+        context: { order },
+      });
+      console.log(`Notification email sent: ${mail}`);
+    } catch (error) {
+      console.error('Error sending notification email:', error);
+    }
   }
 }
